66d23958e712fcbd549923b07fd2df07f69f7ff4
SBoykov <107512405+buyok38@users.noreply.github.com>
2025-01-11 23:10:19 +0300

    Add files via upload

---
+++ lesson-100/main.js
@@ -0,0 +1,16 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+
+
+for (let b = 1; b <= numbers.length; b = b + 2) {
+    console.log(numbers[b]);
+}
+
+
+// ==============================================================================
+
+const rain = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+for (let i = rain.length - 1; i >= 0; i--) {
+	console.log(rain[i]);
+}
