c31183446ae25bcbd14755d997252c6087ee71ec
SBoykov <107512405+buyok38@users.noreply.github.com>
2025-01-11 23:07:30 +0300

    Add files via upload

---
+++ lesson-97/main.js
@@ -0,0 +1,17 @@
+// const age = 26;
+
+// if(age >= 18 ){
+//     const result = ('Вы совершеннолетний!');
+//     console.log(result);
+// } else
+
+// if(age < 18){
+//     const err = 'Вам еще нет 18';
+//     console.log(err);
+// }
+
+// let age = null;
+
+
+const age = 26
+age >= 18 ? console.log('Вы совершеннолетний') : console.log('Вы не совершеннолетний');
